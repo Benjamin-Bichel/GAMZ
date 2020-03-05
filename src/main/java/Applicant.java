3ca77f3a0d852dd245919c5d06651204395fc986
Benjamin Bichel <benjamin.bichel@gmail.com>
2020-03-05 13:30:22 -0500

    Initialization of datamodel

--- src/main/java/Applicant.java
+++ src/main/java/Applicant.java
@@ -1,2 +1,26 @@
-public class Applicant {
+import java.awt.*;
+import java.util.List;
+
+import javax.persistence.*;
+
+@Table
+@Entity
+public class Applicant extends User{
+
+
+
+    private  String email;
+    private String summary;
+    private  String address;
+    private List<Skills> skillsList;
+    private Education education;
+    private List<String> employmentHistory;
+    private List<String> volenteerHistory;
+    private String otherInformation;
+    private boolean adminAproval;
+    private ProfessorRecomendation recomendation;
+    private boolean finalRecomendation;
+    private Project project;
+    private Image deploma;
+
 }
--- src/main/java/Professor.java
+++ src/main/java/Professor.java
@@ -1,2 +1,23 @@
-public class Professor {
+import javax.persistence.*;
+import java.util.List;
+@Table
+@Entity
+public class Professor extends User {
+
+    @ManyToOne
+    private Faculty faculty;
+    //Multiple indexes
+    @OneToMany
+    private List<Project> activeProjectsList;
+    @OneToMany
+    private List<Feild> seekingFeilds;
+    @OneToMany
+    private List<Project> proposedProjects;
+    @ManyToMany
+    private List<Feild> activeFeilds;
+    @ManyToMany
+    private List<Feild> constantFeilds;
+
+
+
 }
