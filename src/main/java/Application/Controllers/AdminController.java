0e3a69ebf233e2570ced780654dcbcb4381d4556
fatimahashi <43560401+fatimahashi@users.noreply.github.com>
2020-04-05 09:43:59 -0400

    Revert "Prof admin applicant relation"

--- src/main/java/Application/Controllers/AdminController.java
+++ src/main/java/Application/Controllers/AdminController.java
@@ -98,9 +98,7 @@ public class AdminController{
     }
 
 
-
-    @RequestMapping(path = "/setFinalDecision", method = RequestMethod.POST)
-
+    @RequestMapping(path = "/setFinalDisition", method = RequestMethod.POST)
 
     public String reviewApplicants(Model model, Applicant applicant) throws RecordNotFoundException {
         System.out.println(applicant);
